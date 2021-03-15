7212dab54197096222a1ef3b1b7b70dc742b0870
Henrik Cheng <henrikcheng@live.se>
2021-03-15 13:41:42 +0100

    btn that changes movie once

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
 import React from "react";
 import "./App.css";
-function App() {
-  return <div className="App">REDUX MOVIELIST APP</div>;
+import { connect } from "react-redux";
+import updateMovies from "./store/actions/updateMovies";
+function App(props) {
+  return (
+    <div className="App">
+      <h3>REDUX MOVIELIST APP</h3>
+      <br />
+      <span style={{ color: "green" }}>YOUR CURRENT MOVIE IS: </span>
+      {props.movies.name}
+      <br />
+      <button onClick={props.updateMovies}>SELECT NEW MOVIE</button>
+    </div>
+  );
 }
-export default App;
+const MapStateToProps = (state) => {
+  return {
+    movies: state.movies,
+  };
+};
+const MapDispatchToProps = (dispatch) => {
+  return {
+    updateMovies: () => dispatch(updateMovies),
+  };
+};
+export default connect(MapStateToProps, MapDispatchToProps)(App);
