668a7ec570ecee91cf82b2efe9ab7e3906a03aa7
Henrik Cheng <henrikcheng@live.se>
2021-03-15 13:52:04 +0100

    added user api

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import { connect } from "react-redux";
 import updateMovies from "./store/actions/updateMovies";
+import fetchUsers from "./store/actions/fetchUsers";
 function App(props) {
   return (
     <div className="App">
@@ -10,18 +11,34 @@ function App(props) {
       <span style={{ color: "green" }}>YOUR CURRENT MOVIE IS: </span>
       {props.movies.name}
       <br />
-      <button onClick={props.updateMovies}>SELECT NEW MOVIE</button>
+      <p>
+        <button onClick={props.updateMovies}>SELECT NEW MOVIE</button>
+      </p>
+      <br />
+      {props.users.length === 0 ? (
+        <p>THERE ARE NO USERS</p>
+      ) : (
+        props.users.map((user) => (
+          <p key={user.id}>
+            {user.first_name} - {user.email}
+          </p>
+        ))
+      )}
+      <br />
+      <button onClick={props.fetchUsers}>FETCH USERS</button>
     </div>
   );
 }
 const MapStateToProps = (state) => {
   return {
     movies: state.movies,
+    users: state.users,
   };
 };
 const MapDispatchToProps = (dispatch) => {
   return {
     updateMovies: () => dispatch(updateMovies),
+    fetchUsers: () => dispatch(fetchUsers),
   };
 };
 export default connect(MapStateToProps, MapDispatchToProps)(App);
